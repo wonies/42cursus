665a5974032ecf3f070d248f2461b3ad5cd669f3
wonies <bluewoniee@gmail.com>
2023-07-15 23:32:47 +0900

    miniedit

--- 7.minishell/minishell_edit/srcs/new_lexer.c
+++ 7.minishell/minishell_edit/srcs/new_lexer.c
@@ -32,7 +32,7 @@ void	input_token(t_data *data, t_token **token, int *i)
 	else if (data->input[*i] == '\'' || data->input[*i] == '\"')
 		tokenization(data, token, i);
 	else if	(data->input[*i] == '$')
-		env_dollor(data, token, i);
+		env_dollor(data, *token, i);
 	else
 	{
 		(*token)->str = ft_strncat((*token)->str, &data->input[*i], 1);
