e1f286ed0d4a61c1dbc9f255ba760bcd9766a95f
WONIE <wonie@WONIEui-MacBookPro.local>
2024-09-16 15:25:14 +0900

    minishell

--- 7.minishell/functions/ctrldhandling.c
+++
@@ -1,8 +0,0 @@
-#include "minifunction.h"
-
-struct termios new_term;
-
-int main(void)
-{
-	tcgetattr(STDIN_FILENO, &new_term);
-}
--- 7.minishell/minishell_edit/srcs/err.c
+++
@@ -1,7 +0,0 @@
-#include "../includes/minishell.h"
-
-void	syntax_err(char *msg)
-{
-	printf("%s\n", msg);
-	exit(2);
-}
